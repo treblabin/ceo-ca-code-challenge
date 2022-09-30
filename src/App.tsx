293497f08e728ec25daed94c510cb1b03f135000
treblabin <treblabin@gmail.com>
2022-09-30 17:16:30 -0400

    FEAT: Update and TableView Btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "./shared/redux/Hooks";
 import { getBitcoinPrice } from "./shared/redux/Crypto";
+import { ChangeViewButton, UpdateButton } from "./elements/Buttons";
 
 const App = () => {
   const dispatch = useAppDispatch();
+  const bitcoinData = useAppSelector((state) => state.Crypto);
+  const [tableView, setTableView] = React.useState(false);
   React.useEffect(() => {
     dispatch(getBitcoinPrice());
   }, []);
@@ -11,6 +15,20 @@ const App = () => {
   return (
     <div>
       <h1>ee</h1>
+        <ChangeViewButton
+          onClick={() => {
+            setTableView(!tableView);
+          }}
+        >
+          Change View Type
+        </ChangeViewButton>
+        <UpdateButton
+          onClick={() => {
+            dispatch(getBitcoinPrice());
+          }}
+        >
+          Update Data
+        </UpdateButton>
     </div>
   );
 };
