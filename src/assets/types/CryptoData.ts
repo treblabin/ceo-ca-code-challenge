c25804d1f9a1f1ee14126f6920ededfb03297c58
treblabin <treblabin@gmail.com>
2022-09-30 05:21:18 -0400

    FEAT: Redux Toolkit Setting

---
+++ src/assets/types/CryptoData.ts
@@ -0,0 +1,36 @@
+export interface CryptoDataType {
+  bpi: {
+    EUR: {
+      code: string;
+      description: string;
+      rate: string;
+      rate_float: number;
+      symbol: string;
+    };
+    GBP: {
+      code: string;
+      description: string;
+      rate: string;
+      rate_float: number;
+      symbol: string;
+    };
+    USD: {
+      code: string;
+      description: string;
+      rate: string;
+      rate_float: number;
+      symbol: string;
+    };
+  };
+  chartName: string;
+  disclaimer: string;
+  time: {
+    updated: string;
+    updatedISO: string;
+    updateduk: string;
+  };
+}
+
+export interface ReduxCryptoDataType {
+  value: CryptoDataType;
+}
